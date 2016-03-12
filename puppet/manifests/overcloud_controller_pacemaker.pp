4453c3d5a2c36cfaa1846c32deb0a2d2b8fc4ded
Qasim Sarfraz <qasims@plumgrid.com>
2016-03-12 05:52:29 +0500

    Minor constraint fix
    Signed-off-by: Qasim Sarfraz <qasims@plumgrid.com>

--- puppet/manifests/overcloud_controller_pacemaker.pp
+++ puppet/manifests/overcloud_controller_pacemaker.pp
@@ -658,7 +658,7 @@ if hiera('step') >= 3 {
     exec { 'pg_lib metadata subnet':
     command => "openstack-config --set /etc/neutron/plugins/plumgrid/plumlib.ini PLUMgridMetadata nova_metadata_subnet ${metadata_sub}",
     path    => [ '/usr/local/bin/', '/bin/' ],
-    notify => Service[$::neutron::params::server_service],
+    before => Class['::neutron'],
     }
 
     exec { 'pg_lib connector_type':

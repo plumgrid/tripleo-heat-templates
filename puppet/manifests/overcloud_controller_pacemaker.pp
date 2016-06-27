cb77ec5826fa6b0567dd96753825954b7a589d38
Abdul Qadeer Khan <abdulq@plumgrid.com>
2016-06-27 02:32:13 -0700

    Update plumlib.ini to support python-keystoneclient versions 1.7.0 and above
    Updated plumlib.ini to support python-keystoneclient versions 1.7.0 and above
    
    Change-Id: I7c2b77d18ceac9739434948eba56e3ccb70abec9
    Ticket: [SOL-1105]
    Signed-off-by: Abdul Qadeer Khan <abdulq@plumgrid.com>

--- puppet/manifests/overcloud_controller_pacemaker.pp
+++ puppet/manifests/overcloud_controller_pacemaker.pp
@@ -822,6 +822,22 @@ if hiera('step') >= 3 {
     before => Class['::neutron'],
     }
 
+    # Configure new parameters for python-keystoneclient versions 1.7.0 and above
+    $user_domain_name = 'Default'
+    exec { 'keystone_authtoken user_domain_name config':
+    command => "openstack-config --set /etc/neutron/plugins/plumgrid/plumlib.ini keystone_authtoken user_domain_name ${user_domain_name}",
+    path    => [ '/usr/local/bin/', '/bin/' ],
+    before => Class['::neutron'],
+    }
+
+    # Configure new parameters for python-keystoneclient versions 1.7.0 and above
+    $project_domain_name = 'Default'
+    exec { 'keystone_authtoken project_domain_name config':
+    command => "openstack-config --set /etc/neutron/plugins/plumgrid/plumlib.ini keystone_authtoken project_domain_name ${project_domain_name}",
+    path    => [ '/usr/local/bin/', '/bin/' ],
+    before => Class['::neutron'],
+    }
+
     # Install PLUMgrid Director
     class{'plumgrid':
       plumgrid_ip => $plumgrid_director_ips,
